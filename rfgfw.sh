040463141f813e401f0ece2bcb6d1c25bdc8d76b
yaroot <yaroot@gmail.com>
2011-05-15 13:03:33 +0800

    .

---
+++ rfgfw.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#   this script make use of ssh tunnel to break the GREAT FIREWALL
+#   each time this script excuted will respawn all the tunnel
+#   remember to use a key for authorize (and keychain if using passphase)
+#
+#   install [autossh] and create a config file: $HOME/.fuckgfw.rc
+#   example:
+#       SSH_SRV="john@192.168.0.254"
+#       FORWARD_PORTS=(7070 7071 7072)
+#
+
+# kill all running autosshs first
+killall autossh 2>/dev/null
+
+source $HOME/.fuckgfw.rc
+
+for port in ${FORWARD_PORTS[@]}; do
+    # -M 0      no listening
+    # -f        run background
+    # -NC       no shell, compress data transfer
+    autossh -M 0 -f -NCD ${port} ${SSH_SRV}
+done
+
+
