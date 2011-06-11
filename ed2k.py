fd12c8f1ee13b9cc813027875f85ddb797fce9b9
yaroot <yaroot@gmail.com>
2011-06-12 03:09:35 +0800

    .

--- ed2k.py
+++ ed2k.py
@@ -4,16 +4,16 @@ import re, urllib2, sys
 def get(link):
     content = urllib2.urlopen(link).read()
     match = re.findall(r'"(ed2k://\|file\|[^\"]+)"', content, re.S)
-    print match
-    links = list()
+
+    all_links = list()
     for link in match:
-        if link in links:
+        if link in all_links:
             continue
         else:
-            links.append(link)
-            print link
+            all_links.append(link)
 
-
+    for link in all_links:
+        print link
 
 
 if __name__ == '__main__':
