9c185d97e2eabd83b7b5bc35614199a2551523e6
yaroot <yaroot@gmail.com>
2011-09-17 20:14:36 +0800

    .

--- imgur.sh
+++ imgur.sh
@@ -4,10 +4,17 @@
 API_KEY="b3625162d3418ac51a9ee805b1840452"
 API_URL="http://imgur.com/api/upload.xml"
 
-FILE_NAME=$1
 
-RESPONSE=$(curl -F "key=$API_KEY" -F "image=@$FILE_NAME" $API_URL)
+imgur_upload(){
+    FILE_NAME=$1
+    RESPONSE=$(curl -F "key=$API_KEY" -F "image=@$FILE_NAME" $API_URL)
 
-echo "$RESPONSE" | grep -E -o '<original_image>(.+)</original_image>' | grep -E -o 'http://i.imgur.com/[^<]*'
-#echo "$RESPONSE" | grep -E -o '<delete_page>(.+)</delete_page>' | grep -E -o 'http://imgur.com/[^<]*'
+    echo "$RESPONSE" | grep -E -o '<original_image>(.+)</original_image>' | grep -E -o 'http://i.imgur.com/[^<]*'
+    #echo "$RESPONSE" | grep -E -o '<delete_page>(.+)</delete_page>' | grep -E -o 'http://imgur.com/[^<]*'
+}
+
+for img in "$@"; do
+    imgur_upload "$img"
+done
+
 
