0bf8e47ed9e3ebfd1545e728bc4ce52809dc6896
yaroot <yaroot@gmail.com>
2012-05-06 15:41:06 +0800

    .

--- setups/git_install.sh
+++ setups/git_install.sh
@@ -9,7 +9,7 @@ LOCAL_CLONE="$2"
 if [ ! -d "$LOCAL_CLONE" ]; then
     mkdir -p "$LOCAL_CLONE"
 fi
-pushd "$LOCAL_CLONE"
+pushd "$LOCAL_CLONE" > /dev/null
 
 #if [ `git rev-parse --git-dir > /dev/null 2>&1` ]; then
 if [ -d '.git' ]; then
@@ -22,5 +22,5 @@ else
     git branch --set-upstream master origin/master
 fi
 
-popd
+popd > /dev/null
 
---
+++ setups/install_n.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./git_install.sh git://github.com/visionmedia/n.git /home/yaroot/repos/n
+
