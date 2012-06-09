a0f693f1df32b50128e8bd13088b49e4759bd566
yaroot <yaroot@gmail.com>
2012-06-09 17:00:34 +0800

    .

--- setups/git_install.sh
+++ setups/git_install.sh
@@ -7,19 +7,21 @@ LOCAL_CLONE="$2"
 
 
 if [ ! -d "$LOCAL_CLONE" ]; then
-    mkdir -p "$LOCAL_CLONE"
+  mkdir -p "$LOCAL_CLONE"
 fi
 pushd "$LOCAL_CLONE" > /dev/null
 
 #if [ `git rev-parse --git-dir > /dev/null 2>&1` ]; then
 if [ -d '.git' ]; then
-    git pull
+  git pull
 else
-    git init
-    git remote add origin "$REPO_URL"
-    git fetch origin
-    git reset --hard origin/master
-    git branch --set-upstream master origin/master
+  git clone "$REPO_URL" .
+
+#  git init
+#  git remote add origin "$REPO_URL"
+#  git fetch origin
+#  git reset --hard origin/master
+#  git branch --set-upstream master origin/master
 fi
 
 popd > /dev/null
