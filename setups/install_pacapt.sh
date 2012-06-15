5bbc432219916b2499463e9b3d717d4293b671e7
yaroot <yaroot@gmail.com>
2012-06-15 14:47:56 +0800

    .

--- setups/install_pacapt.sh
+++ setups/install_pacapt.sh
@@ -3,6 +3,15 @@
 PACAPT_DIR="$HOME/repos/pacapt"
 ./git_install.sh git://github.com/icy/pacapt.git $PACAPT_DIR
 
+
+if [ -f "/etc/os-release" ]; then
+  source /etc/os-release
+  if [ $ID = 'arch' ]; then
+    echo "Error: you're running archlinux :)"
+    exit 0
+  fi
+fi
+
 cd /usr/bin; sudo ln -sv "${PACAPT_DIR}/pacapt/pacman" .
 
 
