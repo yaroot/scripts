1417f56fad7316cd31a4cc7491fc3328190943b9
yaroot <yaroot@gmail.com>
2012-07-04 13:44:41 +0800

    .

--- git_svn_update.sh
+++ git_svn_update.sh
@@ -17,16 +17,19 @@ main(){
   if [ ! -z "$1" ]; then
     branch="$1"
   fi
-  local remote_branch="$SVN_PREFIX/$branch"
-  echo "Branch: $branch"
-  echo "Remote branch: $remote_branch"
+
+  local has_stash=false
+  if [ ! "x`git status --porcelain`" = "x" ]; then
+    has_stash=true
+    run "git stash"
+  fi
 
   if [ "x`git config --get svn-remote.svn.url`" = "x" ]; then
     # git repo
     run "git fetch $REMOTE_NAME"
-    run "git rebase $remote_branch"
+    run "git rebase $REMOTE_NAME/$branch"
 
-    run "git merge $remote_branch"
+    run "git merge $REMOTE_NAME/$branch"
     run "git push $REMOTE_NAME"
   else
     # git-svn repo
@@ -39,6 +42,10 @@ main(){
     run "git svn dcommit"
 
     run "git push $REMOTE_NAME --all -f"
+  fi
+
+  if $has_stash; then
+    run "git stash apply"
   fi
 }
 
