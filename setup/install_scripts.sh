d7a73ff3c550a00945f24667488eaf06b0373294
yaroot <yaroot@gmail.com>
2012-07-15 23:37:59 +0800

    .

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env bash
 
-symlink() {
-  local src="$1"
-  local tar="$2"
-
-  mkdir -p `dirname "$tar"`
-  rm -f "$tar"
-  ln -sv "$src" "$tar"
-}
-
-# n
-./git_install.sh git://github.com/visionmedia/n.git $HOME/repos/n
-mkdir -p $HOME/.nodes/bin
-
-# rbenv
-./git_install.sh git://github.com/sstephenson/rbenv.git $HOME/.rbenv
-./git_install.sh git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
-./git_install.sh git://github.com/jamis/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
-
-# leiningen
-./git_install.sh git://github.com/technomancy/leiningen.git $HOME/repos/leiningen
-symlink $HOME/repos/leiningen/zsh_completion.zsh $HOME/._zshcomp/_lein
-
-# virtualenv
-./git_install.sh git://github.com/pypa/virtualenv.git $HOME/repos/virtualenv
-
 # youtube-dl
 ./git_install.sh git://github.com/rg3/youtube-dl.git $HOME/repos/youtube-dl
 
@@ -32,6 +7,7 @@ symlink $HOME/repos/leiningen/zsh_completion.zsh $HOME/._zshcomp/_lein
 ./git_install.sh git://github.com/zsh-users/zsh-completions.git $HOME/repos/zsh-completions
 mkdir -p $HOME/._zshcomp
 
+./git_install.sh git://github.com/yaroot/etc.git $HOME/repos/etc
 
 download_file_into() {
   local uri="$1"
