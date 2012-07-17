7a0309af9fa21c217b4e00ab33941a96c954e165
yaroot <yaroot@gmail.com>
2012-07-17 23:01:47 +0800

    .

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+symlink() {
+  local src="$1"
+  local tar="$2"
+
+  mkdir -p `dirname "$tar"`
+  rm -f "$tar"
+  ln -sv "$src" "$tar"
+}
+
+# n
+./git_install.sh git://github.com/visionmedia/n.git $HOME/repos/n
+mkdir -p $HOME/.nodes/{bin,lib,include}
+
+# rbenv
+./git_install.sh git://github.com/sstephenson/rbenv.git $HOME/.rbenv
+./git_install.sh git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+./git_install.sh git://github.com/jamis/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
+
+# leiningen
+./git_install.sh git://github.com/technomancy/leiningen.git $HOME/repos/leiningen
+symlink $HOME/repos/leiningen/zsh_completion.zsh $HOME/._zshcomp/_lein
+
+# virtualenv
+./git_install.sh git://github.com/pypa/virtualenv.git $HOME/repos/virtualenv
+
 # youtube-dl
 ./git_install.sh git://github.com/rg3/youtube-dl.git $HOME/repos/youtube-dl
 
