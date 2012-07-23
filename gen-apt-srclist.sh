fb8ddaa702705f5d8975a4e386a73708375a8b06
yaroot <yaroot@gmail.com>
2012-07-23 16:26:23 +0800

    .

--- gen-apt-srclist.sh
+++ gen-apt-srclist.sh
@@ -2,41 +2,41 @@
 
 gen_list() {
   local base="$1"
-  local code="$2"
+  local rel="$2"
   local comp='main contrib non-free'
 
 
   echo ''
   echo ''
 
-  echo "deb http://$base/debian $base $comp"
-  echo "deb-src http://$base/debian $base $comp"
+  echo "deb http://$base/debian $rel $comp"
+  echo "deb-src http://$base/debian $rel $comp"
   echo ''
 
-  echo "deb http://$base/debian ${base}-updates $comp"
-  echo "deb-src http://$base/debian ${base}-updates $comp"
+  echo "deb http://$base/debian ${rel}-updates $comp"
+  echo "deb-src http://$base/debian ${rel}-updates $comp"
   echo ''
 
-  echo "deb http://$base/debian-security $base/updates $comp"
-  echo "deb-src http://$base/debian-security $base/updates $comp"
+  echo "deb http://$base/debian-security $rel/updates $comp"
+  echo "deb-src http://$base/debian-security $rel/updates $comp"
   echo ''
 
-  echo "deb http://$base/debian-backports ${base}-backports $comp"
+  echo "deb http://$base/debian-backports ${rel}-backports $comp"
   echo ''
 
-  echo "deb http://security.debian.org/ ${base}/updates $comp"
-  echo "deb-src http://security.debian.org/ ${base}/updates $comp"
+  echo "deb http://security.debian.org/ ${rel}/updates $comp"
+  echo "deb-src http://security.debian.org/ ${rel}/updates $comp"
   echo ''
 
-  echo "deb http://backports.debian.org/debian-backports ${base}-backports $comp"
+  echo "deb http://backports.debian.org/debian-backports ${rel}-backports $comp"
   echo ''
 }
 
 case $1 in
-  jaist) gen_list 'http://ftp.jaist.ac.jp/pub/Linux' 'squeeze' ;;
-  osuosl) gen_list 'http://mirrors.osuosl.org' 'squeeze' ;;
-  kernel) gen_list 'http://mirrors.us.kernel.org' 'squeeze' ;;
-  nchc) gen_list 'http://ftp.nchc.org.tw' 'squeeze' ;;
-  163) gen_list 'http://mirrors.163.com' 'squeeze' ;;
+  jaist) gen_list 'ftp.jaist.ac.jp/pub/Linux' 'squeeze' ;;
+  osuosl) gen_list 'mirrors.osuosl.org' 'squeeze' ;;
+  kernel) gen_list 'mirrors.us.kernel.org' 'squeeze' ;;
+  nchc) gen_list 'ftp.nchc.org.tw' 'squeeze' ;;
+  163) gen_list 'mirrors.163.com' 'squeeze' ;;
 esac
 
