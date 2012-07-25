16844a8237054ea026cc07c80817106c9b00f89b
yaroot <yaroot@gmail.com>
2012-07-25 17:31:21 +0800

    .

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -9,26 +9,25 @@ symlink() {
   ln -sv "$src" "$tar"
 }
 
-# n
+
 ./git_install.sh git://github.com/visionmedia/n.git $HOME/repos/n
 mkdir -p $HOME/.nodes/{bin,lib,include}
 
-# rbenv
+
 ./git_install.sh git://github.com/sstephenson/rbenv.git $HOME/.rbenv
 ./git_install.sh git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 ./git_install.sh git://github.com/jamis/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
 
-# leiningen
+
 ./git_install.sh git://github.com/technomancy/leiningen.git $HOME/repos/leiningen
 symlink $HOME/repos/leiningen/zsh_completion.zsh $HOME/._zshcomp/_lein
 
-# virtualenv
-./git_install.sh git://github.com/pypa/virtualenv.git $HOME/repos/virtualenv
+# ./git_install.sh git://github.com/pypa/virtualenv.git $HOME/repos/virtualenv
 
-# youtube-dl
+
 ./git_install.sh git://github.com/rg3/youtube-dl.git $HOME/repos/youtube-dl
 
-# zsh-completion
+
 ./git_install.sh git://github.com/zsh-users/zsh-completions.git $HOME/repos/zsh-completions
 mkdir -p $HOME/._zshcomp
 
