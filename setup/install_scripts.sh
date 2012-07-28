48d1b0ab115e99c4f4e694e9ca38811d64abf5e1
yaroot <yaroot@gmail.com>
2012-07-29 02:59:14 +0800

    .

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -9,43 +9,34 @@ symlink() {
   ln -sv "$src" "$tar"
 }
 
-
-./git_install.sh git://github.com/visionmedia/n.git $HOME/repos/n
-mkdir -p $HOME/.nodes/{bin,lib,include}
-
+./git_install.sh git://github.com/visionmedia/n.git $HOME/.local/n
 
 ./git_install.sh git://github.com/sstephenson/rbenv.git $HOME/.rbenv
 ./git_install.sh git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 ./git_install.sh git://github.com/jamis/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
 
-
 ./git_install.sh git://github.com/technomancy/leiningen.git $HOME/repos/leiningen
-symlink $HOME/repos/leiningen/zsh_completion.zsh $HOME/._zshcomp/_lein
+symlink $HOME/repos/leiningen/zsh_completion.zsh $HOME/.local/zshcomp/_lein
 
 # ./git_install.sh git://github.com/pypa/virtualenv.git $HOME/repos/virtualenv
 
-
 ./git_install.sh git://github.com/rg3/youtube-dl.git $HOME/repos/youtube-dl
 
-
-./git_install.sh git://github.com/zsh-users/zsh-completions.git $HOME/repos/zsh-completions
-mkdir -p $HOME/._zshcomp
-
-./git_install.sh git://github.com/yaroot/etc.git $HOME/repos/etc
+./git_install.sh git://github.com/zsh-users/zsh-completions.git $HOME/.local/zsh-completions
 
 download_file_into() {
   local uri="$1"
   local path="$2"
   local filename=$(basename "$uri")
+  mkdir -p $path
   curl -sL "$uri" | tee "${path}/${filename}" > /dev/null
 }
 
-download_file_into http://gemi.fedorapeople.org/haskell/_ghc   "$HOME/._zshcomp"
-download_file_into http://gemi.fedorapeople.org/haskell/_hugs  "$HOME/._zshcomp"
-download_file_into http://gemi.fedorapeople.org/haskell/_cabal "$HOME/._zshcomp"
+ZSHCOMP="$HOME/.local/zshcomp"
+download_file_into http://gemi.fedorapeople.org/haskell/_ghc   $ZSHCOMP
+download_file_into http://gemi.fedorapeople.org/haskell/_hugs  $ZSHCOMP
+download_file_into http://gemi.fedorapeople.org/haskell/_cabal $ZSHCOMP
 
-
-# pacapt
 PACAPT_DIR="$HOME/repos/pacapt"
 ./git_install.sh git://github.com/icy/pacapt.git $PACAPT_DIR
 # sudo ln -sv "${PACAPT_DIR}/pacapt/pacman" /usr/bin/pacman
