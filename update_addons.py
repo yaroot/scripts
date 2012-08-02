b9295503bbdd4bd7f43f2baa84ee032db9c28190
yaroot <yaroot@gmail.com>
2012-08-02 15:12:09 +0800

    .

--- update_addons.py
+++ update_addons.py
@@ -3,22 +3,95 @@
 from __future__ import absolute_import, print_function, unicode_literals, division
 
 import requests
+import re
+
+STATUS_FAIL = -1
+STATUS_OK = 0
+log = dict()
 
 def append_path(a, b):
     return a.rstrip('/')+'/'+b
 
-def get_wowace_file_link_from_download_page(page):
+def init_log():
+    try:
+        with open('addon_update.log', 'r') as f:
+            for line in f.lines:
+                log[line.strip()] = True
+    except IOError as e:
+        pass
+
+def is_file_downloaded(file_name):
+    return log.has_key(file_name)
+
+
+def download_with_fileinfo(info):
+    file_name = info['file_name']
+    link = info['link']
+    hash_ = info['hash']
+    hash_type = info['hash_type']
+    if is_file_downloaded(file_name):
+        return STATUS_OK
+
+
+
+
+wowace_download_link_re = re.compile(r'href="(.+)"')
+def parse_dlpage_wowace(page):
     for line in page.splitlines():
         if line.find('<td class="col-file">') > 0:
-            print(line)
-            return
+            ma = wowace_download_link_re.search(line)
+            if ma:
+                group = ma.groups()
+                if len(group) == 1:
+                    return group[0]
+
+
+wowace_filename_re = re.compile(r'<dd><a href="(.+)">(.+)</a></dd>')
+wowace_md5_re = re.compile(r'<dd>(.+)</dd>')
+def parse_file_info_wowace(page):
+    info = dict()
+    found = 'nothing'
+    for line in page.splitlines():
+        if found != 'nothing':
+            if found == 'filename':
+                match = wowace_filename_re.search(line)
+                if match:
+                    info['link'], info['file_name'] = match.groups()
+            elif found == 'md5':
+                match = wowace_md5_re.search(line)
+                if match:
+                    info['hash'] = match.groups()[0]
+                    info['hash_type'] = 'md5'
+            found = 'nothing'
+        else:
+            if line.find('<dt>Filename</dt>') > 0:
+                found = 'filename'
+            elif line.find('<dt>MD5</dt>') > 0:
+                found = 'md5'
+        if info.has_key('file_name') and info.has_key('link'
+                ) and info.has_key('hash'):
+            return info
 
 
 def download_wowace(link):
     files_page = append_path(link, 'files')
+    site = 'http://www.wowace.com'
+    if link.find('curseforge.com') > 0:
+        site = 'http://wow.curseforge.com'
     r = requests.get(files_page)
-    if(r.status_code == requests.codes.ok):
-        download_page_link = get_wowace_file_link_from_download_page(r.text)
+    if(r.status_code != requests.codes.ok):
+        return STATUS_FAIL
+    download_page_link = parse_dlpage_wowace(r.text)
+    if download_page_link is None:
+        return STATUS_FAIL
+
+    r = requests.get(site+download_page_link)
+    if(r.status_code != requests.codes.ok):
+        return STATUS_FAIL
+    file_info = parse_file_info_wowace(r.text)
+    if file_info:
+        download_with_fileinfo(file_info)
+
 
 def download_curse(link):
     pass
@@ -57,6 +130,7 @@ def parse_config(cfg_file):
 
 
 def main(argv):
+    init_log()
     # addon_list = parse_config('addon.cfg')
     download('http://wow.curseforge.com/addons/heal-bot-continued/')
     download('http://www.wowinterface.com/downloads/info20546-CurrencyMonitor.html')
