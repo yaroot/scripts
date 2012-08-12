f6507bb3c2ed94fa2d75b1caa540f508ea91b04b
yaroot <yaroot@gmail.com>
2012-08-13 03:17:39 +0800

    .

--- update_addons.py
+++ update_addons.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf-8
 """
 this script reads a config and install/update wow addons for you
 
--- vcard2abook.py
+++ vcard2abook.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding=utf-8
 """
 I hereby put this file in the public domain.
 """
