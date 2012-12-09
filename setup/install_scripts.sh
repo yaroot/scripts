47c0fe26dba57edb95e3017e393692041e1eed5a
yaroot <yaroot@gmail.com>
2012-12-09 16:53:05 +0800

    .

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -16,7 +16,7 @@ symlink() {
 
 ./git_install.sh git://github.com/sstephenson/rbenv.git $HOME/.rbenv
 ./git_install.sh git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
-./git_install.sh git://github.com/jamis/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
+# ./git_install.sh git://github.com/jamis/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
 symlink $HOME/.rbenv/completions/rbenv.zsh $HOME/.local/zshcomp/_rbenv
 
 ./git_install.sh git://github.com/technomancy/leiningen.git $HOME/repos/leiningen
