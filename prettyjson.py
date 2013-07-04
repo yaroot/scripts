463da0d261b1d7d261a4347cec59d9205f7e95f4
yaroot <yaroot@gmail.com>
2013-07-04 09:03:33 +0800

    .

---
+++ prettyjson.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+
+from __future__ import unicode_literals
+import simplejson
+import sys
+
+MAGIC_BOMB = '\uFEFF'
+
+def main():
+    raw = sys.stdin.read()
+    if raw[0] == MAGIC_BOMB:
+        raw = raw[1:]
+
+    j = simplejson.loads(raw)
+    s = simplejson.dumps(j, indent=2, ensure_ascii=False)
+    sys.stdout.write(s.encode('utf-8'))
+    sys.stdout.flush()
+
+if __name__ == '__main__':
+    main()
+
