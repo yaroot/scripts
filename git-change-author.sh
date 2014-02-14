641458f18f81a5b5a590f7d94a140dbb9ba11773
yaroot <tsu@yaroot.net>
2014-02-14 13:29:08 +0800

    .

--- git-change-author.sh
+++ git-change-author.sh
@@ -7,12 +7,7 @@ GIT_WORK_EMAIL=''
 
 source $HOME/._git_user_conf
 
-#git filter-branch --env-filter '
-
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
+git filter-branch --env-filter '
 
 if [ ` echo "$GIT_AUTHOR_EMAIL" | grep "$GIT_PERSONAL_TOKEN" > /dev/null `  ]; then
     export GIT_AUTHOR_NAME="$GIT_WORK_NAME"
@@ -24,24 +19,5 @@ if [ ` echo "$GIT_COMMITTER_EMAIL" | grep "$GIT_PERSONAL_TOKEN" > /dev/null `  ]
     export GIT_COMMITTER_EMAIL="$GIT_WORK_EMAIL"
 fi
 
-
-
-# if [ "$GIT_COMMITTER_EMAIL" = "yaroot@gmail.com" ]
-# then
-#     cn="Your New Committer Name"
-#     cm="Your New Committer Email"
-# fi
-# if [ "$GIT_AUTHOR_EMAIL" = "yaroot@gmail.com" ]
-# then
-#     an="Your New Author Name"
-#     am="Your New Author Email"
-# fi
-
-
-
-#export GIT_AUTHOR_NAME="su_yan"
-#export GIT_AUTHOR_EMAIL="su_yan@corp.netease.com"
-#export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-#export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 '
 
