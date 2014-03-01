6da40f71ffc29decb0e95aef920d05b59e5b2db9
yaroot <tsu@yaroot.net>
2014-03-01 20:10:10 +0800

    .

--- git-change-author.sh
+++ git-change-author.sh
@@ -9,12 +9,14 @@ source $HOME/._git_user_conf
 
 git filter-branch --env-filter '
 
-if [ ` echo "$GIT_AUTHOR_EMAIL" | grep "$GIT_PERSONAL_TOKEN" > /dev/null `  ]; then
+
+
+if [ -n ` echo "$GIT_AUTHOR_EMAIL" | grep "$GIT_PERSONAL_TOKEN" `  ]; then
     export GIT_AUTHOR_NAME="$GIT_WORK_NAME"
     export GIT_AUTHOR_EMAIL="$GIT_WORK_EMAIL"
 fi
 
-if [ ` echo "$GIT_COMMITTER_EMAIL" | grep "$GIT_PERSONAL_TOKEN" > /dev/null `  ]; then
+if [ -n ` echo "$GIT_COMMITTER_EMAIL" | grep "$GIT_PERSONAL_TOKEN" `  ]; then
     export GIT_COMMITTER_NAME="$GIT_WORK_NAME"
     export GIT_COMMITTER_EMAIL="$GIT_WORK_EMAIL"
 fi
