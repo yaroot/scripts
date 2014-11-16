1346b6a719e5cd231cb1ed33560850527b8b0490
toni <tsu@yaroot.net>
2014-11-16 16:15:15 +0800

    .

--- daemonize.py
+++ daemonize.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python2
+# TODO
+# - handle arguments to execlp new program
+# - should be rewrite in c to be used in production?
 
 import os, signal
 
-def daemonize(args):
-    if os.fork() == 0:
-        os.setsid()
-        signal.signal(signal.SIGHUP, signal.SIG_IGN)
+def daemonize(umask=None, jail_dir=None, uid=None, gid=None):
+    pid = os.fork()
 
-        if os.fork() == 0:
-            prog = args.pop()
-            argv = tuple(args)
-            # os.execl(prog, argv)
-            os.execlp(prog, argv)
-        else:
-            os._exit(0)
-    else:
+    # fork failed
+    if pid < 0:
+        raise RuntimeError('os.fork() failed')
+
+    # terminate parent
+    if pid > 0:
         os._exit(0)
 
+    os.setsid()
+
+    # ignore signals
+    signal.signal(signal.SIGHUP, signal.SIG_IGN)
+    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
+    pid = os.fork()
+    if pid < 0:
+        raise RuntimeError('os.fork() failed')
+
+    # terminate parent
+    if pid > 0:
+        os._exit(0)
+
+    if umask:
+        os.umask(umask)
+
+    if jail_dir:
+        os.chroot(jail_dir)
+        os.chdir('/')
+
+    if uid and gid and os.getuid == 0:
+        os.setgroups([])
+        os.setgid(gid)
+        os.setuid(uid)
+
+
 if __name__ == '__main__':
-
-
+    daemonize()
