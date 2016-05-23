4091588744c299fb6d2d2e7ff74a9a6eb38389c6
tsu <tsu@yaroot.net>
2016-05-23 21:17:20 +0800

    .

---
+++ dump_docker_images_from_v1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function, unicode_literals, division
+
+import sys
+import requests
+
+session = requests.session()
+
+
+def dump_all_images(base_url):
+    r = session.get("%s/v1/search" % base_url)
+    for image in r.json()['results']:
+        yield image['name']
+
+
+def dump_all_tags(base_url, image):
+    r = session.get("%s/v1/repositories/%s/tags" % (base_url, image))
+    for tag, _ in r.json().items():
+        yield tag
+
+
+def dump_all_images_with_tags(registry_base):
+    sys.stderr.write('> fetching image list')
+    for image in dump_all_images(registry_base):
+        sys.stderr.write('> fetching tags for %s' % image)
+        for tag in dump_all_tags(registry_base, image):
+            sys.stdout.write('%s:%s\n' % (image, tag))
+
+
+def main():
+    if len(sys.argv) == 2:
+        dump_all_images_with_tags(sys.argv[1].rstrip("/"))
+    else:
+        print("Usage: dump.py http(s)://your.registry:port")
+
+if __name__ == '__main__':
+    main()
