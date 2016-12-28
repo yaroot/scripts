277ad86c876dcbbfa15c55f0975ff197fdcc36f1
tsu <tsu@yaroot.net>
2016-12-28 18:34:12 +0800

    .

---
+++ circe_gen.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+
+def camalize(name):
+    if len(name) > 1:
+        return name[0].lower() + name[1:]
+    else:
+        return name.lower()
+
+
+def derive(name, what):
+    n = camalize(name)
+    return 'implicit val %s%s: %s[%s] = derive%s[%s]' % (
+        n, name, what, name, what, name
+    )
+
+def main():
+    import sys
+    for name in sys.argv[1:]:
+        print derive(name, 'Encoder')
+        print derive(name, 'Decoder')
+
+if __name__ == '__main__':
+    main()
