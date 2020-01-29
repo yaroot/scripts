feb9729a6ebfba9a4a876dcefca444e520359282
Yan Su <tsu@yaroot.net>
2020-01-29 12:09:58 +0900

    .

--- server-mgmt/node-apt-update.sh
+++ server-mgmt/node-apt-update.sh
@@ -2,9 +2,8 @@
 
 node="$1"
 
-APT_ARGS="--force-yes --assume-yes"
+APT_ARGS="--assume-yes"
 
-./remote.sh -t $node sudo apt-get update '&&' sudo apt-get upgrade ${APT_ARGS} '&&' sudo apt-get dist-upgrade ${APT_ARGS} '&&' sudo apt-get clean ${APT_ARGS}
-
-
+./remote.sh -t $node sudo apt-get update '&&' sudo apt-get upgrade ${APT_ARGS} '&&' sudo apt-get autoclean ${APT_ARGS}
+# sudo apt-get dist-upgrade ${APT_ARGS}
 
