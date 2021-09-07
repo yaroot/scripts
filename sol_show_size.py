ba92e5ff506efc88dd9446095b363c78c2e56b93
Yan Su <tsu@yaroot.net>
2021-09-07 17:42:53 +0900

    .

---
+++ sol_show_size.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import json
+import sys
+
+
+def main(path):
+    with open(path, 'r') as f:
+        sol_out = json.load(f)
+    print('bytecode size:', len(bytes.fromhex(sol_out['bytecode'][2:])))
+    print('bytecode (deployed) size:', len(bytes.fromhex(sol_out['deployedBytecode'][2:])))
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
