01d628b3022d524235f9f76354a431556be2fcb1
Yan <tsu@yaroot.net>
2022-01-25 23:41:00 +0900

    .

--- sol_show_size.py
+++ sol_show_size.py
@@ -7,10 +7,11 @@ import sys
 def main(path):
     with open(path, 'r') as f:
         sol_out = json.load(f)
+    print('---', path)
     print('bytecode size:', len(bytes.fromhex(sol_out['bytecode'][2:])))
     print('bytecode (deployed) size:', len(bytes.fromhex(sol_out['deployedBytecode'][2:])))
     pass
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    list(map(main, sys.argv[1:]))
