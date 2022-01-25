639111c86a886516b43ee83c098d8a30db3edd8c
Yan <tsu@yaroot.net>
2022-01-25 23:44:13 +0900

    .

--- sol_show_size.py
+++ sol_show_size.py
@@ -7,9 +7,16 @@ import sys
 def main(path):
     with open(path, 'r') as f:
         sol_out = json.load(f)
-    print('---', path)
-    print('bytecode size:', len(bytes.fromhex(sol_out['bytecode'][2:])))
-    print('bytecode (deployed) size:', len(bytes.fromhex(sol_out['deployedBytecode'][2:])))
+    print(
+        '---',
+        path,
+        '\t',
+        'bytecode size',
+        len(bytes.fromhex(sol_out['bytecode'][2:])),
+        '\t'
+        'deployed size',
+        len(bytes.fromhex(sol_out['deployedBytecode'][2:])),
+    )
     pass
 
 
